7b18c5dc4bad898f634616360b85c246cb5fbb59
Mariia Boldyreva <mboldyreva@cloudlinux.com>
2021-09-14 23:06:14 +0300

    Implementing pagination & auth redirec 403

--- src/pages/BuildFeed.vue
+++ src/pages/BuildFeed.vue
@@ -8,30 +8,59 @@
         style="margin-top: 1vw;"
       />
     </div>
+    <div class="q-pa-lg flex flex-center">
+      <q-pagination input :max="totalPages" v-model="currentPage" :disable="loading"/>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import BuildFeedItem from 'components/BuildFeedItem.vue';
+import { defineComponent, ref } from 'vue'
+import BuildFeedItem from 'components/BuildFeedItem.vue'
+import { Loading } from 'quasar'
 
 export default defineComponent({
   name: 'BuildFeed',
   data () {
     return {
-      builds: []
+      builds: [],
+      totalPages: ref(1),
+      loading: false
     }
   },
   created () {
     this.loadFeedPage()
   },
+  computed: {
+    buildFeedQuery () {
+      return this.$store.getters['buildsFeed/buildFeedQuery']
+    },
+    currentPage: {
+      get () { return this.$store.state.buildsFeed.pageNumber },
+      set (value) {
+        this.loading = true
+        this.$store.commit('buildsFeed/setPageNumber', value)
+        this.loadFeedPage()
+      }
+    }
+  },
   methods: {
     loadFeedPage () {
-      this.$api.get('/builds/')
+      this.loading = true
+      Loading.show()
+      this.$api.get(`/builds/`, {params: this.buildFeedQuery})
         .then(response => {
-          this.builds = response.data
+          this.loading = false
+          Loading.hide()
+          this.builds = response.data['builds']
+          this.totalPages = Math.ceil(response.data['total_builds'] / 10)
         })
         .catch(error => {
+          this.loading = false
+          Loading.hide()
+          if (error.response.status === 403) {
+            this.$router.push('/auth/login')
+          }
           // TODO: add error here
         })
     }
