6bc269fa5d6e01f08f25b97c5a0843d83069eb50
maccelf <maxmol27@gmail.com>
2022-05-24 11:06:43 +0200

    ALBS-373 Add error during create/update release

--- src/pages/CreateRelease.vue
+++ src/pages/CreateRelease.vue
@@ -39,6 +39,7 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
+import { Notify } from 'quasar'
 import BuildSelectionForm from 'components/BuildSelectionForm.vue'
 import PackageLocationSelectionForm from 'components/PackageLocationSelectionForm.vue'
 
@@ -64,6 +65,14 @@ export default defineComponent({
                 })
                 .catch(error => {
                     console.log(error)
+                    Notify.create({
+                        message: `${error.response.status}: ${error.response.statusText}`,
+                        type: 'negative',
+                        actions: [
+                            { label: 'Dismiss', color: 'white', handler: () => {} }
+                        ]
+                    })
+                    this.onPreviousStep()
                 })
         },
         updateRelease (request_body) {
@@ -74,6 +83,14 @@ export default defineComponent({
                 })
                 .catch(error => {
                     console.log(error)
+                    Notify.create({
+                        message: `${error.response.status}: ${error.response.statusText}`,
+                        type: 'negative',
+                        actions: [
+                            { label: 'Dismiss', color: 'white', handler: () => {} }
+                        ]
+                    })
+                    this.onPreviousStep()
                 }) 
         },
         saveState (state){
