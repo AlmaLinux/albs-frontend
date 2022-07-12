e2ff5b377321be3d289f809c76e5699d5b2d75e9
maccelf <maxmol27@gmail.com>
2022-07-12 12:43:21 +0200

    Add try catch block for parse JWT functionality

--- src/pages/Github.vue
+++ src/pages/Github.vue
@@ -4,7 +4,7 @@
 
 <script>
 import { defineComponent } from 'vue'
-import { Cookies, LocalStorage } from 'quasar'
+import { Cookies, LocalStorage, Notify } from 'quasar'
 import { parseJwt } from '../utils'
 
 export default defineComponent({
@@ -12,15 +12,28 @@ export default defineComponent({
   
   created () {
     let token = Cookies.get('albs')
-    let user = {
-      user_id: parseJwt(token).user_id,
-      jwt_token: token
-    }
-    this.$store.commit('users/updateSelf', user)
-    let redirectPath = LocalStorage.getItem('redirectPath')
-    if (redirectPath.startsWith('/auth')) redirectPath = '/'
+    try {
+      let parsedToken = parseJwt(token)
+      let user = {
+        user_id: parsedToken.user_id,
+        jwt_token: token
+      }
+      this.$store.commit('users/updateSelf', user)
+      let redirectPath = LocalStorage.getItem('redirectPath')
+      if (redirectPath.startsWith('/auth')) redirectPath = '/'
 
-    this.$router.push(redirectPath)
+      this.$router.push(redirectPath)
+    } catch (err) {
+      console.log(err)
+      Notify.create({
+          message: 'Authorization error: Invalid JWT',
+          type: 'negative',
+          actions: [
+              { label: 'Dismiss', color: 'white', handler: () => {} }
+          ]
+      })
+      this.$router.push('/')
+    }
   }
 })
 </script>
