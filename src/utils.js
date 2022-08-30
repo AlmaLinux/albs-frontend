9b86d18ea88ee796cf063421521a19bfe04fdb4c
Javier Hernández <jhernandez@cloudlinux.com>
2022-08-30 18:00:13 +0200

    ALBS-595: Added utility to get a diff of two JSON objects

--- src/utils.js
+++ src/utils.js
@@ -85,3 +85,38 @@ export function parseJwt (jwt_token) {
   }).join(''))
   return JSON.parse(jsonPayload)
 }
+
+
+/**
+ * Difference between two JSON Objects
+ *
+ * A function that returns the differences between two JSON objects
+ * Taken from: https://stackoverflow.com/a/8432188
+ * Can be improved and doesn't handle corner cases but it does the job
+ * for our purposes.
+ *
+ * @param {Object} Object A
+ * @param {Object} Object B
+ * @returns {Object}
+ */
+export function diff(obj1, obj2) {
+    const result = {};
+    if (Object.is(obj1, obj2)) {
+        return undefined;
+    }
+    if (!obj2 || typeof obj2 !== 'object') {
+        return obj2;
+    }
+    Object.keys(obj1 || {}).concat(Object.keys(obj2 || {})).forEach(key => {
+        if(obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
+            result[key] = obj2[key];
+        }
+        if(typeof obj2[key] === 'object' && typeof obj1[key] === 'object') {
+            const value = diff(obj1[key], obj2[key]);
+            if (value !== undefined) {
+                result[key] = value;
+            }
+        }
+    });
+    return result;
+}
