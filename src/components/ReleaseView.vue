ba56c84aab2c69e2e89bd951c061d1332e3c8c6f
maccelf <maxmol27@gmail.com>
2022-09-15 10:44:09 +0200

    ALBS-630 Minor improvements for releases

--- src/components/ReleaseView.vue
+++ src/components/ReleaseView.vue
@@ -1,162 +1,98 @@
 <template>
-  <q-dialog position="top" v-model="opened" full-height full-width>
-        <q-card style="max-width: 100%;">
-            <q-form style="max-width: 100%;">
-                <q-card-section class="bg-primary shadow-2">
-                    <div class="text-h6 text-white" style="text-align: center;">Release info</div>
-                </q-card-section>
-                <q-card-section style="width: 100%; margin-left: auto; margin-right: auto;">
-                    <q-tabs
-                        v-model="tab"
-                        class="text-primary"      
-                    >
-                        <q-tab  name="summary" label="Summary" />
-                        <!-- <q-tab  name="errata" label="Errata/OVAL" /> -->
-                        <q-tab  name="pack_list" label="Packages list"/>
-                        <q-tab  name="logs" label="Release Logs" />
-                    </q-tabs>
+    <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+    >
+        <q-tab  name="urls" label="Publication urls" icon="cloud_sync"/>
+        <!-- <q-tab  name="errata" label="Errata/OVAL" /> -->
+        <q-tab  name="pack_list" label="Packages list" icon="toc"/>
+        <q-tab  name="logs" label="Logs" icon="sticky_note_2"/>
+    </q-tabs>
 
-                    <q-tab-panels v-model="tab" animated style="width: 100%; height: 100%;">
-                        <q-tab-panel name="summary">
-                            <div class="q-pa-md" style="max-width: 80%; margin-left: auto; margin-right: auto;">
-                                <q-markup-table>
-                                    
-                                <thead>
-                                    <tr>
-                                        <th class="text-left">Platform</th>
-                                        <th class="text-left">Status</th>
-                                        <th class="text-left">Publication urls
-                                            <div class="q-pr-sm" style="padding-top: 5px; position: absolute; right: 0; top: 0;" >
-                                                <q-btn  @click="commitRelease"
-                                                        color="primary"
-                                                        v-if="release.status === 1"
-                                                        :loading="loading">
-                                                    Commit
-                                                </q-btn>
-                                            </div>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td class="text-left">{{ release.platform.name }}</td>
-                                        <td class="text-left">
-                                            <q-chip
-                                                :color="statusColor(release)"
-                                                text-color="white"
-                                                dense
-                                                class="text-weight-bolder"
-                                                square
-                                                >{{ releaseStatus.text[release.status] }}
-                                            </q-chip>
-                                        </td>
-                                        <td class="text-left">
-                                            <a class="row" v-for="url in publicationUrls()" :key="url" :href="url">
-                                                {{url}}
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                                </q-markup-table>
-                            </div>
-                        </q-tab-panel>
+    <q-tab-panels v-model="tab" animated style="width: 100%; height: 100%;">
+        <q-tab-panel name="urls">
+            <div class="q-pa-md" style="max-width: 90%; margin-left: auto; margin-right: auto;">
+                <q-markup-table flat>
+                    
+                <thead>
+                    <tr>
+                        <th class="text-left">Name</th>
+                        <th class="text-left">Arch</th>
+                        <th class="text-center">Url</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="repo in publicationUrls()" :key="repo.id">
+                        <td class="text-left">{{ repo.name }}</td>
+                        <td class="text-left">{{ repo.arch }}</td>
+                        <td class="text-center">
+                            <a :href="repo.url">
+                                {{repo.url}}
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+                </q-markup-table>
+            </div>
+        </q-tab-panel>
 
-                        <!-- <q-tab-panel name="errata">
-                            <div class="text-h6">Errata/OVAL</div>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        </q-tab-panel> -->
+        <!-- <q-tab-panel name="errata">
+            <div class="text-h6">Errata/OVAL</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel> -->
 
-                        <q-tab-panel name="pack_list" style="max-width: 100%;">
-                            <q-scroll-area style="height: 600px;">
-                                <package-location-selection-form :release="release" :viewOnly="true"/>
-                            </q-scroll-area>
-                        </q-tab-panel>
+        <q-tab-panel name="pack_list" style="max-width: 100%;">  
+            <package-location-selection-form :release="release" :viewOnly="true"/>
+        </q-tab-panel>
 
-                        <q-tab-panel name="logs">
-                            <div class="q-pt-none log-container">
-                                <pre>
-                                    {{ release.plan.last_log ? `${release.plan.last_log}`: "This release hasn't produced any logs yet" }}
-                                </pre>
-                            </div>
-                        </q-tab-panel>
+        <q-tab-panel name="logs">
+            <div class="row justify-center q-pt-none log-container">
+                <pre>
+                    {{ release.plan.last_log ? `${release.plan.last_log}`: "This release hasn't produced any logs yet" }}
+                </pre>
+            </div>
+        </q-tab-panel>
 
-                    </q-tab-panels>
-                </q-card-section>
-
-                <q-card-actions align="right">
-                    <q-btn flat label="close" color="primary" v-close-popup />
-                </q-card-actions>
-
-            </q-form>
-        </q-card>
-    </q-dialog>
-
+    </q-tab-panels>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue'
-import { Notify} from 'quasar'
-import { ReleaseStatus } from 'src/constants'
 import PackageLocationSelectionForm from 'components/PackageLocationSelectionForm.vue'
 
 export default defineComponent({
+    props: {
+        release: Object,
+    },
     data () {
         return {
             opened: false,
-            tab: 'summary',
-            release: null,
-            releaseStatus: ReleaseStatus,
+            tab: ref('urls'),
             loading: false
         }
     },
     methods: {
-        open(release) {
-            this.release = release
-            this.opened = true
-        },
-        close() {
-            this.opened = false
-        },
         publicationUrls () {
             let repoUrl = new Set
+            let repos = []
             let packages = this.release.plan.packages
             if (packages) {
                 packages.forEach(pack => {
                     pack.repositories.forEach(repo => {
-                        if (repo.url) repoUrl.add(repo.url)
+                        if (repo.url && !repoUrl.has(repo.url)){
+                            repos.push(repo)
+                            repoUrl.add(repo.url)
+                        }
                     })
                 });
             }
-            return Array.from(repoUrl)
-        },
-        statusColor(release){
-            let col = ''
-            switch (release.status) {
-                case this.releaseStatus.SCHEDULED:
-                    col = 'grey'
-                    break;
-                case this.releaseStatus.IN_PROGRESS:
-                    col = 'primary'
-                    break;
-                case this.releaseStatus.COMPLETED:
-                    col = 'green'
-                    break;
-                case this.releaseStatus.FAILED:
-                    col = 'negative'
-                    break;
-            }
-            return col
-        },
-        commitRelease () {
-            this.loading = true
-            this.$api.post(`/releases/${this.release.id}/commit/`)
-                .then(response => {
-                    this.loading = false
-                    Notify.create({message: response.data.message, type: 'positive',
-                        actions: [{ label: 'Dismiss', color: 'white', handler: () => {} }]})
-                    this.$router.push(`/release-feed`)
-                })
-        },
+            return repos
+        }
     },
     components: {
         PackageLocationSelectionForm
@@ -169,10 +105,14 @@ export default defineComponent({
     font-size: small;
     overflow-y: auto;
     padding-left: 2em;
+    background-color: #f5f8fa !important;
   }
 
   .log-container pre {
     white-space: pre-line;
     word-break: break-all;
+    color: #0c5176 !important;
+    font-family: Consolas,Monaco,Andale Mono,Ubuntu Mono,monospace;
+    text-align: left;
   }
 </style>
