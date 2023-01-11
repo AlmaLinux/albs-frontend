0ab5fb3a096c1a1d45f5979ad2a4c0c6153192e7
maccelf <maxmol27@gmail.com>
2023-01-11 17:02:22 +0100

    ALBS-912 Fix name parsing

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ import {Notify} from 'quasar'
  */
 export function splitRpmFileName(url) {
   const re =
-    /(?:\/|^)([\w\-\+|.]+?)-([\w\^~.]+?)-([\w.+]+?)\.(src|noarch|x86_64|i[3456]86)\.rpm$/i
+    /(?:\/|^)([\w\-\+|.]+?)-([\w\^\+~.]+?)-([\w.+]+?)\.(src|noarch|x86_64|i[3456]86)\.rpm$/i
   const rslt = url.match(re)
   if (rslt) {
     return {
