d1e2014e9c3fc50431e2f6e1fdca9f012d0f2db6
maccelf <maxmol27@gmail.com>
2023-01-24 15:14:59 +0100

    Prettier syntax

--- src/pages/Github.vue
+++ src/pages/Github.vue
@@ -1,41 +1,41 @@
 <template>
-  <div/>
+  <div />
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { Cookies, LocalStorage, Notify } from 'quasar'
-import { parseJwt } from '../utils'
+  import { defineComponent } from 'vue'
+  import { Cookies, LocalStorage, Notify } from 'quasar'
+  import { parseJwt } from '../utils'
 
-export default defineComponent({
-  name: 'PostLogin-page',
-  
-  created () {
-    let token = Cookies.get('albs')
-    try {
-      let parsedToken = parseJwt(token)
-      let user = {
-        user_id: parsedToken.user_id,
-        jwt_token: token
+  export default defineComponent({
+    name: 'PostLogin-page',
+
+    created () {
+      let token = Cookies.get('albs')
+      try {
+        let parsedToken = parseJwt(token)
+        let user = {
+          user_id: parsedToken.user_id,
+          jwt_token: token
+        }
+        this.$store.commit('users/updateSelf', user)
+        let redirectPath = LocalStorage.getItem('redirectPath')
+        if (redirectPath.startsWith('/auth')) redirectPath = '/'
+
+        this.$store.dispatch('users/setIsAdmin').then(isAdmin => {
+          this.$router.push(redirectPath)
+        })
+      } catch (err) {
+        console.log(err)
+        Notify.create({
+            message: 'Authorization error: Invalid JWT',
+            type: 'negative',
+            actions: [
+                { label: 'Dismiss', color: 'white', handler: () => {} }
+            ]
+        })
+        this.$router.push('/')
       }
-      this.$store.commit('users/updateSelf', user)
-      let redirectPath = LocalStorage.getItem('redirectPath')
-      if (redirectPath.startsWith('/auth')) redirectPath = '/'
-
-      this.$store.dispatch('users/setIsAdmin').then(isAdmin => {
-        this.$router.push(redirectPath)
-      })
-    } catch (err) {
-      console.log(err)
-      Notify.create({
-          message: 'Authorization error: Invalid JWT',
-          type: 'negative',
-          actions: [
-              { label: 'Dismiss', color: 'white', handler: () => {} }
-          ]
-      })
-      this.$router.push('/')
     }
-  }
-})
+  })
 </script>
