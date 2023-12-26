c84e75783dad238075f545bd202b3cde2f331007
Javier Hernández <jhernandez@cloudlinux.com>
2023-12-26 14:54:31 +0100

    Add login support for AlmaLinux Accounts
    Resolves https://github.com/AlmaLinux/build-system/issues/138

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,14 +1,39 @@
 <template>
-  <div class="flex flex-center login-button">
-    <q-btn
-        round
-        flat
-        :icon="fabGithub"
-        text="Login with github"
-        @click="githubLogin"
-        size="64px"
-    >
-    </q-btn>
+  <div class="container-fluid absolute-center">
+    <div class="row">
+      <div class="col-md-12">
+        <h5 class="text-center">
+          Please select the service you want to use to log into ALBS
+        </h5>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6" style="display: grid;">
+        <q-btn
+          flat
+          text="Login with AlmaLinux Accounts"
+          @click="almalinuxLogin"
+        >
+          <q-icon class="text-dark" size="250px">
+            <img src="~assets/almalinux-logo.png" alt="Login using your AlmaLinux account"/>
+          </q-icon>
+        </q-btn>
+      </div>
+
+      <div class="col-md-6" style="display: grid;">
+        <q-btn
+          flat
+          text="Login with your Github profile"
+          @click="githubLogin"
+        >
+          <q-icon class="text-dark" size="250px">
+            <img src="~assets/github-mark.svg" alt="Login using your Github profile"/>
+          </q-icon>
+        </q-btn>
+      </div>
+
+
+    </div>
   </div>
 </template>
 
@@ -31,6 +56,12 @@ export default defineComponent({
         .then((response) => {
           window.location.href = response.data.authorization_url
         })
+    },
+    almalinuxLogin () {
+      this.$api.get('/auth/almalinux/authorize')
+        .then((response) => {
+          window.location.href = response.data.authorization_url
+        })
     }
   }
 })
@@ -38,6 +69,14 @@ export default defineComponent({
 
 <style>
   .login-button{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -50px;
+    margin-left: -50px;
+  }
+
+  .login-box{
     position: absolute;
     top: 50%;
     left: 50%;
