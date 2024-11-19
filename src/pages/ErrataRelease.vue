4ccaee3a7b4e280c0a2ef441c485a4efa34612e5
Javier Hernández <jhernandez@cloudlinux.com>
2024-11-19 13:10:41 +0100

    Prettier pages/ErrataRelease.vue

--- src/pages/ErrataRelease.vue
+++ src/pages/ErrataRelease.vue
@@ -1,41 +1,40 @@
 <template>
-    <div class="q-pa-md">
-        <package-location-selection-form ref="packageLocationSelectionForm"/>
-    </div>
+  <div class="q-pa-md">
+    <package-location-selection-form ref="packageLocationSelectionForm" />
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import PackageLocationSelectionForm from 'components/PackageLocationSelectionForm.vue'
-import { Notify } from 'quasar'
-import { useRoute } from 'vue-router'
+  import {defineComponent} from 'vue'
+  import PackageLocationSelectionForm from 'components/PackageLocationSelectionForm.vue'
+  import {Notify} from 'quasar'
+  import {useRoute} from 'vue-router'
 
-export default defineComponent({
-    created () {
-        this.createRelease()
+  export default defineComponent({
+    created() {
+      this.createRelease()
     },
     methods: {
-        createRelease() {
-            const route = useRoute()
-            let data = JSON.parse(route.query.requestBody)
-            this.$api.post(`/releases/new/`, data)
-            .then(response => {
-                this.$refs.packageLocationSelectionForm.createTable(response.data)
+      createRelease() {
+        const route = useRoute()
+        let data = JSON.parse(route.query.requestBody)
+        this.$api
+          .post(`/releases/new/`, data)
+          .then((response) => {
+            this.$refs.packageLocationSelectionForm.createTable(response.data)
+          })
+          .catch((error) => {
+            Notify.create({
+              message: `${error.response.status}: ${error.response.statusText}`,
+              type: 'negative',
+              actions: [{label: 'Dismiss', color: 'white', handler: () => {}}],
             })
-            .catch(error => {
-                Notify.create({
-                    message: `${error.response.status}: ${error.response.statusText}`,
-                    type: 'negative',
-                    actions: [
-                        { label: 'Dismiss', color: 'white', handler: () => {} }
-                    ]
-                })
-                this.$router.go(-1)
-            })
-        }
+            this.$router.go(-1)
+          })
+      },
     },
     components: {
-        PackageLocationSelectionForm
-    }
-})
+      PackageLocationSelectionForm,
+    },
+  })
 </script>
